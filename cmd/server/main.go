4fb5b69dbc2e96d17806736a615ce852daa99d1f
Timur Gilyazov <progwayoftimur@gmail.com>
2025-01-26 19:58:47 +0300

    Edited dsn(make .env file and parse dsn information from this file)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"log"
+	"os"
 	"to-do-list-go/internal/database"
 	"to-do-list-go/internal/models"
 	"to-do-list-go/internal/router"
 )
 
 func main() {
-	dsn := "host=localhost user=postgres password=yourpassword dbname=postgres port=5432 sslmode=disable"
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Ошибка загрузки .env файла: %v", err)
+	}
+	dsn := "host=" + os.Getenv("DB_HOST") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" port=" + os.Getenv("DB_PORT") +
+		" sslmode=" + os.Getenv("DB_SSLMODE")
+
 	db := database.InitDB(dsn)
 
 	database.MigrateDB(db, &models.Message{})
